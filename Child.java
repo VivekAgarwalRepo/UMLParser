b4bf6a629b4c0b00a6444ed99282ef593fbe3963
VivekAgarwalRepo <vivek.d.agarwal@sjsu.edu>
2017-03-11 09:35:09 -0800

    Child class methods defined

--- Child.java
+++ Child.java
@@ -1,14 +1,21 @@
 class Child implements D,E{
+    int a,b;
+    Child(){
+        a=0;
+        b=0;
+    }
     private int getMembers(){
-
+        return a+b;
     }
     private void setMembers(){
-
+        a=-1;
+        b=-1;
     }
     int getAttributes(){
-
+        return a*b;
     }
     void setAttributes(){
-        
+        a=1;
+        b=1;
     }
 }

1ce3847bb0a2cb6b162601e7a163f3cceee106e6
VivekAgarwalRepo <vivek.d.agarwal@sjsu.edu>
2017-03-10 18:48:25 -0800

    Modificatiof for base class - Getters and Setters

--- Test.java
+++ Test.java
@@ -1,10 +1,16 @@
 class A{
+    int a,b;
 
+    public void getVariables();
+    public void setVaraibles(int x,int y){
+        this.a=x;
+        this.b=y;
+    }
 }
 
 class B extends A{
     int x,y;
-    A(){
+    B(){
         x=0;
         y=0;
     }

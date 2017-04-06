d93ad292128448f38231c8189077126dfba59c89
VivekAgarwalRepo <vivek.d.agarwal@sjsu.edu>
2017-04-06 15:08:29 -0700

    extends class and implements interface

--- Test2.java
+++ Test2.java
@@ -1,9 +1,14 @@
 class Test2{
 }
 
-class B extends Test2{
+class B extends Test2,implements E{
     C c;
     B(C obj){
         this.c=C;
     }
+
+    int getAttributes(){}
+    void setAttributes(){}
 }
+
+

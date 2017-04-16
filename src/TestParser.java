26ac6c53bb15c050eaa1038e89340239c1759095
VivekAgarwalRepo <vivek.d.agarwal@sjsu.edu>
2017-04-15 20:04:41 -0700

    Extend relationship

---
+++ src/TestParser.java
@@ -0,0 +1,19 @@
+import com.github.javaparser.*;
+
+public class TestParser {
+
+    public static void main(String[] args) throws Exception {
+        // creates an input stream for the file to be parsed
+        FileInputStream in = new FileInputStream("test.java");
+
+        // parse the file
+        CompilationUnit cu = JavaParser.parse(in);
+
+        // prints the resulting compilation unit to default system output
+        System.out.println(cu.toString());
+    }
+}
+
+class testParser2 extends TestParser{
+    
+}

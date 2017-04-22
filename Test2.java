c3b094c38bffc8b7af0dfe24355b00cf36483339
VivekAgarwalRepo <vivek.d.agarwal@sjsu.edu>
2017-04-22 16:09:13 -0700

    Excess member attributes discarded

--- Test2.java
+++ Test2.java
@@ -3,11 +3,6 @@ class Test2{
 }
 
 class B extends Test2,implements E{
-    C c;
-    B(C obj){
-        this.c=C;
-    }
-
     int getAttributes(){}
     void setAttributes(){}
     public void display(){}
